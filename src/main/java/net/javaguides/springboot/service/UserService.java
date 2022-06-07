d871b676fe59452ffd68d2f953657e8071d907f8
00ZRHun <00zrhun@gmail.com>
2022-06-07 12:30:18 +0800

    S3: Inject or Autowire ModelMapper

--- src/main/java/net/javaguides/springboot/service/UserService.java
+++ src/main/java/net/javaguides/springboot/service/UserService.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.service;
 import net.javaguides.springboot.dto.UserLocationDTO;
 import net.javaguides.springboot.model.User;
 import net.javaguides.springboot.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public List<UserLocationDTO> getAllUsersLocation(){
         return userRepository.findAll()
